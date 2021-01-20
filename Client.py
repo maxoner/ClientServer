573107565b0f1d1037212cea7ba4828819fa7bd4
Максим Панин <maxon1999Pain@gmail.com>
2021-01-20 12:09:13 +0300

    initial

---
+++ Client.py
@@ -0,0 +1,94 @@
+"""
+Клиент отправки метрик
+"""
+import bisect
+import re
+import socket
+from time import time
+
+
+class ClientError(Exception):
+    pass
+
+
+class Client:
+
+    _put_template = 'put {} {} {}\n'
+
+    _get_template = 'get {}\n'
+
+    def __init__(self, host, port, timeout=None):
+        try:
+            self.sock = socket.create_connection((host, port), timeout)
+        except socket.error:
+            print("connection failed")
+        self.host = host
+        self.port = port
+        self.timeout1 = timeout
+        
+    def __del__(self):
+        if self.sock:
+            self.sock.close()
+        del self
+
+    def _send_request(self, req_string):
+        """
+        Отправляет строку на сервер по подключенному сокету,
+        предварительно кодируя в байты
+        """
+        try:
+            self.sock.sendall(req_string.encode("utf8"))
+        except socket.error:
+           raise ClientError 
+       
+    def _recv_response(self, buff=4096, cd="utf8"):
+        """
+        получаем данные с сервера, декодируя
+        """
+        data = self.sock.recv(buff)
+        return data.decode(cd)
+
+    def put(self, key, value, timestamp=None):
+        put_req = self._put_template.format(
+            key, value, 
+            timestamp if timestamp else int(time())) 
+        self._send_request(put_req)
+        if self._recv_response() != "ok\n\n":
+            raise ClientError("load fail") 
+
+    def get(self, key):
+        #Посылаем запрос
+        get_req = self._get_template.format(key)
+        self._send_request(get_req)
+        data = self._recv_response() 
+
+        #Обрабатываем
+        data_splitted = data.split('\n')[:-2]
+        if data_splitted[0] == "error":
+            raise ClientError("Wrong request") 
+        elif data_splitted[0] not in {"error", "ok"}:
+            raise ClientError("Wrong response")
+
+        #Записываем в словарь
+        data_dict = {}
+        for line in data_splitted[1:]:      #Отрезаем ok
+            try:
+                key, val, timestamp = re.split(r'\ +', line)
+            except ValueError:
+                if not line:
+                    raise ClientError("Key not exist")
+                raise ClientError("Wrong response")  
+            data_dict.setdefault(key, [])
+            # try: 
+            #     data_dict[key] += [(int(timestamp),float(val))]
+            # except ValueError:
+            #     raise ClientError("Wrong data types in response")
+            # data_dict[key].sort() — неэффективно каждый раз 
+            #                         сортировать весь массив, для этого
+            #                         можно вставлять сортируя, 
+            #                         методом bisect.insort
+            try :
+                bisect.insort(data_dict[key], ((int(timestamp), float(val))))
+            except ValueError:
+                raise ClientError("Wrong types in response")
+        return data_dict
---
+++ Server.py
@@ -0,0 +1,136 @@
+"""
+Сервер для приема и отправки метрик
+"""
+import asyncio
+from collections import defaultdict
+from operator import itemgetter
+import re
+import socket
+import sys
+
+#TODO: обвешать тестами с использованием моего класса Client.
+#TODO: заменить все шаблоны строк на _.format(key,value) чтобы если что заменить
+#в одном месте протокол
+
+#----------------=======Хранилище=======-----------------------------
+class DictInStorage(dict):
+    """
+    Также перегружен метод __str__, 
+    чтобы соответствовать протоколу отправки запросов
+    """
+    def __init__(self, key, *args, **kwargs):
+        self.upperkey = key
+        super().__init__(*args, **kwargs)
+    def __str__(self):
+        return '\n'.join([f"{self.upperkey} {val} {time}" for time, val in self.items() ])
+
+        
+class Storage(defaultdict):
+    """
+    Обёртка класса defaultdict, с перегруженным __str__ чтобы печатать в
+    соответствии с протоколом запроса. В отличие от defaultdict, вызывает
+    производящую функцию с **одним** аргументом, а не с нулем
+    """
+    def __init__(self):
+        super().__init__(DictInStorage)
+
+    def __missing__(self, key):
+        """Вызывается при генерации значения по несуществующему ключу"""
+        val = self.default_factory(key)
+        self.setdefault(key, val)
+        return val 
+
+    def __str__(self): 
+        triples = sorted(
+            [(k,v,t) for k in self.keys() for t, v in self[k].items()], 
+            key=itemgetter(2))
+        return '\n'.join([f"{k} {v} {t}" for k,v,t in triples])
+    
+
+
+storage = Storage()
+
+
+#---------------==========Константы и шаблоны=========-------------------
+PUT_PATTERN = re.compile(r'put \S+ \d+.?\d* \d+\n')
+GET_PATTERN = re.compile(r'get \S+\n')
+BY_SPACE = re.compile(r'\s+')
+RESPONSE_TEMPLATE = "{}\n{}\n\n"
+
+
+#---------------==========Сервер===========-------------------------------
+async def handle_request(reader, writer, buff=4096, cd='utf8'):
+    while True:
+        request = await reader.read(buff)
+        request = request.decode(cd)
+
+        addr = writer.get_extra_info('peername')
+        print(f"Received {request!r} from {addr!r}")
+        if not request:
+            break
+        else:
+            if PUT_PATTERN.match(request):
+                response = handle_put(request)
+            elif GET_PATTERN.match(request):
+                response = handle_get(request)
+            else:
+                response = raise_error()
+
+            print(f"Send {response} at {addr}")
+            writer.write(response.encode())
+            await writer.drain()
+
+    print(f"Closing connection with {addr}")
+    writer.close()
+
+
+def handle_put(request):
+    _, key, value, timestamp, _ = BY_SPACE.split(request)
+    storage[key][int(timestamp)] = float(value)
+    response = 'ok\n\n' 
+    return response 
+
+
+def handle_get(request):
+    _, key, _ = BY_SPACE.split(request)
+
+    if key == '*':
+        response = "ok\n" + f"{storage}" + "\n\n"
+    else:
+        if (cont := storage[key]):
+            response = "ok\n" + f"{cont}"  + "\n\n"
+        else:
+            response = "ok\n\n"
+            x = storage.pop(key)
+            del x
+    return response 
+
+
+def raise_error():
+    return "error\nwrong command\n\n" 
+ 
+
+async def run_server(host, port):
+    """
+    Запускает сервер, который при соединении с ним запускает 
+    корутину handler, передавая ей объекты reader 
+    и writer для асинхронного ввода вывода.
+    """
+    server = await asyncio.start_server(
+                    handle_request, host, 
+                    port, family=socket.AF_INET)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Starting server on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == '__main__':
+    try:
+        host, port = sys.argv[1].split(':')
+    except Exception:
+        hostport = input('host:port ::')
+        host, port = hostport.split(':')
+    asyncio.run(run_server(host, port))
